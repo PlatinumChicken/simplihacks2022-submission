509a87744ffeb43dc195ccc9bec3db3e9a40c69e
PlatinumChicken <raymond.tingrui.huang@gmail.com>
2024-08-09 17:47:38 -0400

    idk

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$k_&1_p7-vhbgo2yu2729%tg4wl8hw9oqv6$ak3&mon7_az_!^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
